87fac999d1e28e7fe406e782a3c5ca7722a6a89b
Combatd <combatdstar@gmail.com>
2022-04-09 16:33:52 -0700

    Fix comments

--- problemSolve/validAnagram.js
+++ problemSolve/validAnagram.js
@@ -1,5 +1,6 @@
 function validAnagram(word1, word2){
-    // add whatever parameters you deem necessary - good luck!    // splits by character, sorts the characters alphabetically, joins them back together
+    // add whatever parameters you deem necessary - good luck!    
+    // splits by character, sorts the characters alphabetically, joins them back together
       if (word1.split('').sort().join() == word2.split('').sort().join()) {
           return true;
       }
