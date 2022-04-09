8be77ba7cde96589a26cb35c6b745053677130da
Combatd <combatdstar@gmail.com>
2022-04-09 16:54:02 -0700

    Define #getUniqueValues

--- problemSolve/countUniqueValues.js
+++ problemSolve/countUniqueValues.js
@@ -1,4 +1,4 @@
-function countUniqueValues(arr) {
+function getUniqueValues(arr) {
     const uniqArr = [];
     for(let i = 0; i < arr.length; i++) {
         if(uniqArr[uniqArr.length - 1] != arr[i]) { // if we don't have the value in the uniqArr already
@@ -6,4 +6,14 @@ function countUniqueValues(arr) {
         }
     }
     return uniqArr;
+}
+
+function countUniqueValues(arr) {
+    const uniqArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(uniqArr[uniqArr.length - 1] != arr[i]) { // if we don't have the value in the uniqArr already
+            uniqArr.push(arr[i]);
+        }
+    }
+    return uniqArr.length;
 }
