e514901195caa7ebc01fb7a450dd9e483f22ced5
Combatd <combatdstar@gmail.com>
2022-04-09 16:30:35 -0700

    Define #validAnagram

---
+++ problemSolve/validAnagram.js
@@ -0,0 +1,7 @@
+function validAnagram(word1, word2){
+    // add whatever parameters you deem necessary - good luck!    // splits by character, sorts the characters alphabetically, joins them back together
+      if (word1.split('').sort().join() == word2.split('').sort().join()) {
+          return true;
+      }
+      return false;
+}
