2621900a0298808fbceb8899506002d48b8767ab
Combatd <combatdstar@gmail.com>
2022-04-09 16:47:03 -0700

    Define #sumZeroBetter

--- problemSolve/sumZero.js
+++ problemSolve/sumZero.js
@@ -11,3 +11,25 @@ function sumZero(arr) {
 }
 
 console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5]));
+
+const sumZeroBetter = (arr) => {
+    // lets pivot between two numbers, we "reduce" the index on each side of the array
+    let left = 0;
+    let right = arr.length - 1;
+    while(left < right) {
+        let sum = arr[left] + arr[right];
+        if(sum === 0) {
+            return [arr[left], arr[right]];
+            /*
+                const sumPair = [];
+                sumPair.push(arr[left]);
+                sumPair.push(arr[right]);
+            */
+        }
+        else if(sum > 0) {
+            right--; // lets reduce the right side...
+        } else {
+            left--; // lets reduce the left side
+        }
+    }
+}
