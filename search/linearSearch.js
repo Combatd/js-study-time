ee039ca69394704d3bb4505e19a8b8528dc30042
Combatd <combatdstar@gmail.com>
2022-04-30 19:46:20 -0700

    Comment about Linear Searrch

--- search/linearSearch.js
+++ search/linearSearch.js
@@ -14,3 +14,11 @@ console.log(linearSearch([0, 11, 65, 98, 42, 100], 40));
 console.log(linearSearch([0, 11, 65, 98, 42, 100], 0));
 console.log(linearSearch([0, 11, 65, 98, 42, 100], 65));
 console.log(linearSearch([0, 11, 65, 98, 42, 100], 42));
+
+/*
+BIg O Linear Search
+
+O(1) Best Case - We checked the 1st element of the array and it is there!
+O(n) Worst case - We make it to the end of the array and it is finally found.
+O(n) Average case - It could be halfway through the array... O(n/2) = O(n)
+*/
