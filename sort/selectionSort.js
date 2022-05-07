dbd4b6d423cf1fb3eb9118d85a0c6d8c90e73168
Combatd <combatdstar@gmail.com>
2022-05-06 17:09:45 -0700

    Refactor #selectionSor

--- sort/selectionSort.js
+++ sort/selectionSort.js
@@ -6,12 +6,35 @@ We would take the element and compare it to the next element in the array
 until a smaller number is found
 */
 
+/*
 function selectionSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         let lowest = i;
         for (let j = i + 1; j < arr.length; j++) {
             if(arr[j] < arr[lowest]) {
                 lowest = j;
+            }
+            temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+    }
+    return arr;
+}
+*/
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let lowest = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if(arr[j] < arr[lowest]) {
+                lowest = j;
+            }
+            // only swap when other element is lower
+            if(i !== lowest) {
+                temp = arr[i];
+                arr[i] = arr[lowest];
+                arr[lowest] = temp;
             }
             temp = arr[i];
             arr[i] = arr[lowest];
