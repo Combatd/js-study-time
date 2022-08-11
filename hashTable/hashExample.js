b64cd9c5877d0cccf408728217b8b99f8fa137d2
Combatd <combatdstar@gmail.com>
2022-08-11 09:37:26 -0700

    Define #hash"

--- hashTable/hashExample.js
+++ hashTable/hashExample.js
@@ -1,3 +1,6 @@
+// Only Hashes Strings
+// O(n) Time, this won't work well
+// Not random, predictable hash
 function hashExample(key, arrayLen) {
     let total = 0;
     for (let char of key) {
@@ -6,3 +9,17 @@ function hashExample(key, arrayLen) {
     }
     return total;
 }
+
+function hash(key, arrayLen) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+        let char = key[i];
+        let value = char.charCodeAt(0) - 96;
+        total = (total * WEIRD_PRIME + value) % arrayLen;
+    }
+    return total;
+}
+
+console.log(hash('hello', 13));
+console.log(hash('goodbye', 13));
