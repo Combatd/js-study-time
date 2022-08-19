973fc67a5a398457d9a18d3cb3235eadbee874a2
Combatd <combatdstar@gmail.com>
2022-08-19 11:11:59 -0700

    Define #addEdge

--- graph/graphClass.js
+++ graph/graphClass.js
@@ -15,4 +15,22 @@ class Graph {
             this.adjacencyList[vertex] = [];
         }  
     } 
+
+    /*
+        #addEdge
+        Accepts 2 vertices as arguments, vertex1 and vertex2
+        Should find in the adjacencyList the key vertex1, and push vertex2 to the value array. 
+        Should find the adjacencyList the key vertex2, and push vertex1 to the value array.
+    */
+    addEdge = function(vertex1, vertex2) {
+        let adjacencyListVertex1Exists = this.adjacencyList[vertex1];
+        let adjacencyListVertex2Exists = this.adjacencyList[vertex2];
+        if (adjacencyListVertex1Exists) {
+            this.adjacencyList[vertex1].push(vertex2)
+        }
+        if ( adjacencyListVertex2Exists) {
+            this.adjacencyList[vertex2].push(vertex1)
+        }  
+    }
+
 }
