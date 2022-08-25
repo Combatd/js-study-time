40588fe255845e71d7cd16b3a148305248ff0d25
Combatd <combatdstar@gmail.com>
2022-08-25 13:50:00 -0700

    Define #quickSort

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -11,7 +11,7 @@ function pivot(arr, start = 0, end = arr.length - 1) {
     var pivot = arr[start];
     var swapIndex = start; // keep track of what we are swapping the first element with at the very end
 
-    for (let i = start + 1; i < arr.length; i++) {
+    for (let i = start + 1; i <= end; i++) {
         // compare our pivot
         if (pivot > arr[i]) {
             swapIndex += 1;
@@ -23,3 +23,16 @@ function pivot(arr, start = 0, end = arr.length - 1) {
 }
 
 pivot([4, 8, 2, 1, 5, 7, 6, 3]); // => 3
+
+function quickSort(arr, left = 0, right = arr.length - 1) {    
+    if (left < right) {
+        let pivotIndex = pivot(arr, left, right);
+        // left
+        quickSort(arr, left, pivotIndex - 1);
+        // right
+        quickSort(arr, pivotIndex + 1, right);
+    }
+    return arr;
+}
+
+console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
