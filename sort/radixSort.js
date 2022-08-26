0b41152b5ec63c6c51277da43db440eb3dd2f3a7
Combatd <combatdstar@gmail.com>
2022-08-26 01:04:23 -0700

    Define #getDigit

---
+++ sort/radixSort.js
@@ -0,0 +1,24 @@
+/*
+Radix sort
+A special sorting algorithm that works on lists of numbers
+
+It never makes comparisons between elements
+
+It exploits the fact that information about the size of a number is encoded in the number of digits.
+
+More digits means a bigger number
+*/
+
+
+
+/*
+#geDigit(num, place)
+returns the digit in num at the given place value
+*/
+function getDigit(num, place) {
+    let numberDividedByHundredInteger = Math.floor(Math.abs(num) / Math.pow(10, place));
+    return numberDividedByHundredInteger % 10; // base 10
+}
+
+console.log(getDigit(7323, 2)); // => 3
+console.log(getDigit(7323, 12)); // => 0
