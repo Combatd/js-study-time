8db2b23c2e058a926c00ec8310b840e368b2e31d
Combatd <combatdstar@gmail.com>
2022-08-29 23:32:26 -0700

    Defien #push

--- LinkedList/singlyLinkedList.js
+++ LinkedList/singlyLinkedList.js
@@ -17,11 +17,26 @@ class SinglyLinkedList {
         creates a new node using value passed to function
         if no new head property, set head and tail to be new node
         otherwise, next property of tail should be new node, and tail should be new node
+        increment length by 1
+        return the linked list
     */
     push(value) {
-
+        let node = new Node(value);
+        if (!this.head) {
+            this.head = node;
+            this.tail = node; // it could also be 'this.head' since it points to the node in memory
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+        return this;
     }
 }
 
 let first = new Node('Hi');
 let list = new SinglyLinkedList();
+list.push('Hi');
+list.push(99);
+console.log(list.head);
+console.log(list.tail);
