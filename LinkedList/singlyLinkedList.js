e61908d4fc8afd66f5af2b402f9ce653afbba8c3
Combatd <combatdstar@gmail.com>
2022-09-02 16:05:11 -0700

    Define #get

--- LinkedList/singlyLinkedList.js
+++ LinkedList/singlyLinkedList.js
@@ -87,6 +87,25 @@ class SinglyLinkedList {
        }
        return node;
    }
+
+   /*
+    #get
+    This function should accept an index
+    If the index is less than zero or greater than or equal to the length of the list, return null
+    Loop through the list until you reach the index and return the node at that specific index
+   */
+  get = function(index) {
+      if (index < 0 || index >= this.length) {
+          return null;
+      }
+      let counter = 0;
+      let current = this.head;
+      while (counter != index) {
+          current = current.next;
+          counter++;
+      }
+      return current;
+  }
 }
 
 let first = new Node('Hi');
@@ -97,5 +116,6 @@ list.push('!');
 console.log(list.head);
 console.log(list.tail);
 list.traverse();
-list.pop();
+// list.pop();
 console.log(list.shift());
+console.log(list.get(1));
