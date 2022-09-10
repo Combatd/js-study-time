ce4bd6af4fa511bf4e1b883e6295e6722825cd84
Combatd <combatdstar@gmail.com>
2022-09-10 16:30:37 -0700

    Create DoublyLinkedList class

---
+++ LinkedList/doublyLinkedList.js
@@ -0,0 +1,21 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
+
+let first = new Node(12);
+first.next = new Node(13);
+first.next.prev = first;
+first.next.next = new Node(14);
+first.next.next.prev = first.next;
